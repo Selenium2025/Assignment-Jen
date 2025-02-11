9e53d833160a6f1ae38211a46006c7a9cfeb2a64
Selenium2025 <jenhopkinswj2127@gmail.com>
2025-02-11 15:04:02 -0500

    Add files via upload
    pushing code

---
+++ day1/FirstProgram.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+
+//Public -Access Modifier class=Keyword FirstProgram=ClassName
+// main method syntax = public static void main(String args[]){}
+
+public class FirstProgram {
+	
+	public static void main (String arg[]) {
+		System.out.println("Jenhopkins");
+		
+	}
+
+}
